341a46a1e386a428748fe41285f9a5bfd4add1bd
Ларионова Анастасия <98683371+Larionova-Anastassiya@users.noreply.github.com>
2022-02-17 13:24:26 +0600

    Update 6.py

--- Lab3/Function1/6.py
+++ Lab3/Function1/6.py
@@ -1,4 +1,11 @@
+"""
 def reverseWords(s):
   return s[::-1]
 s = input()
 print(reverseWords(s))
+"""
+def reverseWords(s):
+  return s[::-1]
+s = input().split() #слова перевернет местами в предложении, если без сплита то буквы все. Если ввод We are ready
+ans = reverseWords(s) #выведет ready are We
+print(*ans)
