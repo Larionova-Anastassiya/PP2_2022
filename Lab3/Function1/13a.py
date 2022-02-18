5260578125b1d2e5a5b5d5c7e115e52925700e6a
Ларионова Анастасия <98683371+Larionova-Anastassiya@users.noreply.github.com>
2022-02-18 18:45:15 +0600

    Update 13a.py

--- Lab3/Function1/13a.py
+++ Lab3/Function1/13a.py
@@ -1,30 +1,21 @@
-def counter(num):
-    def inner(inp, number):
-        inner.count += 1
-        return num(inp, number)
-    inner.count = 0
-    return inner
-def num(inp,number):
-    #c = 1  # начальный счетчик если угадаем сразу
-    while inp != number:  # условия если не угадали и давать подсказки
-        if inp < number:
-            print("Your guess is too low.")
-        elif inp > number:
-            print("Your guess is too high.")
-        print("Take a guess.")
-        inp = input()
-        inp = int(inp)  # вводим вновь число
-        #c += 1  # счетчик добавляет попытки
+def counter(num, name):
+    c = 0
+    while True:
+        a = int(input())
+        if a == number:
+            c += 1
+            print (f'Good job, {name}! You guessed my number in {c} guesses!" ')
+            exit()
+        if a < number:
+            c += 1
+            print('Your guess is too low.')
+        if a > number:
+            c += 1
+            print('Your guess is too big.')
 import random
 print("Hello! What is your name?")
 name = input() #ввести свое имя для игры
 print ("Well, "  + name + ", I am thinking of a number between 1 and 20." )
 print("Take a guess.")
 number = random.randint(1, 20) #компьютер загадает рандомное число от 1 до 20
-inp = int(input()) #мы вводим число любое
-#c = 1 #начальный счетчик если угадаем сразу
-c = counter(num)
-num(inp, number)
-print (f'Good job, {name}! You guessed my number in {c} guesses!" ') #выведет имя и за сколько попыток
-
-#не работает счетчик попыток (спросить)
+counter(number, name)
