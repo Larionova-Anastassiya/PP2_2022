2aa21aa1089ca46458934c98c7b4428c1ce89330
Ларионова Анастасия <98683371+Larionova-Anastassiya@users.noreply.github.com>
2022-02-26 17:38:32 +0600

    Add files via upload

---
+++ Lab4/math/2.py
@@ -0,0 +1,23 @@
+#calculate the area of a trapezoid.
+print("Height:")
+h = int(input())
+print("Base, first value:")
+f = int(input())
+print("Base, second value:")
+s = int(input())
+print("Expected Output (area of a trapezoid) :")
+ans = (1/2) * h * (f + s)
+print(ans)
+
+"""
+example: 
+
+Height:
+5
+Base, first value:
+5
+Base, second value:
+6
+Expected Output (area of a trapezoid) :
+27.5
+"""
