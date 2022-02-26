4ce2388a3e77bb7c262dd8837d3ea4821e0fbd88
Ларионова Анастасия <98683371+Larionova-Anastassiya@users.noreply.github.com>
2022-02-26 18:08:51 +0600

    Add files via upload

---
+++ Lab4/math/4.py
@@ -0,0 +1,8 @@
+import math
+print("Length of base:")
+l = float(input())
+print("Height of parallelogram:")
+h = float(input())
+print("Expected Output (area of a parallelogram):")
+ans = l * h
+print(ans)
