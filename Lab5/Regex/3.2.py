bdf4c834f34afd09fda14fde05f9cec6f7ecaac0
Ларионова Анастасия <98683371+Larionova-Anastassiya@users.noreply.github.com>
2022-03-07 15:50:13 +0600

    Add files via upload

---
+++ Lab5/Regex/3.2.py
@@ -0,0 +1,22 @@
+#to find sequences of lowercase letters joined with a underscore.
+import re
+
+
+def match(n): #функция выполняющая условия
+    ans = r'(.*[a-z]_[a-z])' #lowercase letters joined with a underscore (aab_abbbc)
+    if re.search(ans, n): #применит к нашему тексту функцию регекса
+        return 'Yes!'
+    else:
+        return ('No!')
+
+
+m = input()
+print(match(m)) #выведет функцию с нашим значением
+
+"""
+Example:
+
+aab_Abbbc -> No!
+Aaab_abbbc -> No!
+aab_cbbbc -> Yes!
+"""
