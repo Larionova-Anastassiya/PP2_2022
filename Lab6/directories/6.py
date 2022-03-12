e733de873209f06c5b9e7f44d4d1ae4072b47532
Ларионова Анастасия <98683371+Larionova-Anastassiya@users.noreply.github.com>
2022-03-12 09:40:31 +0600

    Update 6.py

--- Lab6/directories/6.py
+++ Lab6/directories/6.py
@@ -2,11 +2,10 @@
 import os
 import string
 
-"""
 if not os.path.exists("Letters"): #если нет такой папки
-   os.mkdir("Letters/") #то он создаст папку
-"""
+   os.mkdir("Letters") #то он создаст папку
 
+os.chdir('Letters')
 #создаст отдельно все наши файлы
 for letter in string.ascii_uppercase: #Константное перечисление букв из набора ASCII в верхнем регистре
     with open(letter + ".txt", "w") as f: #создаст файл для всех букв и перезаписывать данные будет
